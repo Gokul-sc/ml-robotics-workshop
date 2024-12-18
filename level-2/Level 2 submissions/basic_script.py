dbc337bb6b97628d11840d20e5e17ce42905766b
Gokul Sree Chandra <polavarapugokul@gmail.com>
2024-12-18 19:29:38 +0530

    Basic python script

---
+++ level-2/Level 2 submissions/basic_script.py
@@ -0,0 +1,7 @@
+# python script that asks the user for the name and prints a greeting 
+# message, and then print the square of a number given by the user
+
+user_input=input("What's your name? ")
+print(f"Hello!, {user_input}")
+number=int(input("Enter a number to find the square of it"))
+print(number**2)
